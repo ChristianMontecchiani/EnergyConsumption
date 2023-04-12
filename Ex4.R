1371072d227f443161f7b1c614edfd13c0415284
Christian Montecchiani <christian.montecchiani99@gmail.com>
2023-04-12 16:19:09 +0200

    Linear Regression model Norm and not norm

--- Ex4.R
+++ Ex4.R
@@ -4,4 +4,107 @@ library(dplyr)
 library("tidyr")
 library(ggplot2)
 library(nnet)
+#install.packages("Metrics")
+library(Metrics)
+# Store the pre-retrofit data set in a variable and start analyzing it
+file_path_preretrofit = "./Desktop/ProgettoR/data/BaselinePeriod.csv"
+BaselinePeriod <- read.csv(file_path_preretrofit)
 
+# Data pre processing:
+# 1) Change date format
+# 2) Factorize the day of the week 
+# 3) Eliminate rows that corresponds to Sunday
+BaselinePeriod$date <- as.POSIXct(BaselinePeriod$date, format="%Y-%m-%d") 
+BaselinePeriod$DayOfTheWeek <- as.factor(BaselinePeriod$DayOfTheWeek)
+BaselinePeriod <- BaselinePeriod[BaselinePeriod$Energy != 0, ]
+View(BaselinePeriod)
+
+# Build linear model without normalization 
+energy_cons_reg <-  lm(Energy  ~ Text, data= BaselinePeriod)
+summary(energy_cons_reg) # Summary contains R^2
+
+# Compute RMSE 
+RMSE <- rmse(actual = BaselinePeriod$Energy, predicted = energy_cons_reg$fitted.values)
+# Compute CVRMSE
+CVRMSE <- (1/mean(BaselinePeriod$Energy)) * RMSE
+# Compute MAPE 
+MAPE <- mape(actual = BaselinePeriod$Energy, predicted = energy_cons_reg$fitted.values)
+
+# Check the norm of the residuals
+residuals <- energy_cons_reg$coefficients
+hist(residuals)
+shapiro.test(residuals)
+plot(residuals, pch = 16, col = "red")
+
+# Plot the model 
+plot(energy_cons_reg)
+
+
+# We can normalize all data that are numeric 
+min_max_scale <- function(data) {
+  for (col in names(data)) {
+    if (is.numeric(data[[col]])) {
+      data[col] <- (data[[col]] - min(data[[col]])) / (max(data[[col]]) - min(data[[col]]))
+    }
+  }
+  return(data)
+}
+
+evaluate_model <- function(data){
+  lin_model <- lm(Energy  ~ Text, data=data)
+  # Compute RMSE 
+  RMSE <- rmse(actual = data$Energy, predicted = lin_model$fitted.values)
+  print(paste("RMSE:", RMSE))
+  # Compute CVRMSE
+  CVRMSE <- (1/mean(data$Energy)) * RMSE
+  print(paste("CVRMSE:", CVRMSE))
+  # Compute MAPE 
+  MAPE <- mape(actual = data$Energy, predicted = lin_model$fitted.values)
+  print(paste("MAPE:", MAPE))
+  
+  
+  return(lin_model)
+}
+# Build linear model with normalization 
+BaselinePeriodNorm <- min_max_scale(BaselinePeriod)
+norm_lm <- evaluate_model(BaselinePeriodNorm)
+lm <- evaluate_model(BaselinePeriod)
+
+# Check the norm of the residuals
+residuals <- norm_lm$coefficients
+hist(residuals)
+shapiro.test(residuals)
+plot(residuals, pch = 16, col = "red")
+
+# Plot the model 
+plot(norm_lm)
+
+# As you can see there is a linear negative relationship between external temperature and energy consumption
+set.seed(100)
+nn <- nnet(Energy~., data=BaselinePeriod, size=4, linout=TRUE, skip=TRUE, MaxNWts=10000, trace=FALSE, maxit=500, )
+# Explain variables 
+# linout  -> TRUE for linear output units. FALSE for logistic output units.
+# skip    -> TRUE to add skip-layer connections from input to output.
+# MaxNWts -> The maximum allowable number of weights. There is no intrinsic limit in the code, but increasing MaxNWts will probably allow fits that are very slow and time-consuming.
+# trace   -> FALSE no tracing optimization. Default TRUE.
+# maxit   -> Max iteration number.
+summary(nn)
+names(nn)
+# Compute RMSE 
+RMSE <- rmse(actual = BaselinePeriod$Energy, predicted = nn$fitted.values)
+# Compute CVRMSE
+CVRMSE <- (1/mean(BaselinePeriod$Energy)) * RMSE
+# Compute MAPE 
+MAPE <- mape(actual = BaselinePeriod$Energy, predicted = nn$fitted.values)
+
+
+# Neural NetWork with normalization
+set.seed(100)
+nn <- nnet(Energy~., data=BaselinePeriodNorm, size=4, linout=TRUE, skip=TRUE, MaxNWts=10000, trace=FALSE, maxit=500, )
+# Explain variables 
+# linout  -> TRUE for linear output units. FALSE for logistic output units.
+# skip    -> TRUE to add skip-layer connections from input to output.
+# MaxNWts -> The maximum allowable number of weights. There is no intrinsic limit in the code, but increasing MaxNWts will probably allow fits that are very slow and time-consuming.
+# trace   -> FALSE no tracing optimization. Default TRUE.
+# maxit   -> Max iteration number.
+summary(nn)
